50d4ff43c05ee791da0f76bd219a2e9a756106a8
lizraeli <leo2002b@yahoo.com>
2017-01-31 16:12:04 -0500

    remove devtools

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { Router, Route, browserHistory } from 'react-router';
-import DevTools from './containers/DevTools'
 import App from './components/App';
-import reducer from './reducers/index'
+import rootReducer from './reducers/index'
 import './index.css';
 
 
 
 
-let store = createStore(reducer, DevTools.instrument())
+let store = createStore(rootReducer)
 
 
 
@@ -21,7 +20,6 @@ ReactDOM.render(
         <Router history={browserHistory}>
           <Route path="/(:title)" component={App}/>
         </Router>
-        <DevTools/>
      </div>
   </Provider>,
   document.getElementById('root')
